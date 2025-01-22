e69d7237b701f4754c410733b787672d8a75b379
HstSethi <hstsethi@outlook.com>
2025-01-22 08:42:49 +0530

    Add support of ages 1-18

--- scripts/get-random-auto-bio-account.sh
+++ scripts/get-random-auto-bio-account.sh
@@ -3,7 +3,7 @@
 # date: 2025-01-19
 # author: HstSethi
 # Posix-sh script to return html of random account from short-autobiography and render it
-randomAge=$(shuf -i 5-18 -n 1)
+randomAge=$(shuf -i 1-18 -n 1)
 
 # While XMLLint does not properly support HTML5, I am using it because it is fast, and has html output
 # --htmlout option seems unnecessary as lynx is still able to render perfectly.
